566a9ca8c0e76f9e1faed93ce6cdb57f39f6bdf5
andyp <andy.pandaan@outlook.com>
2025-05-31 17:21:45 +0400

    working on part5

--- main.go
+++ main.go
@@ -1,153 +1,17 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"os"
-	"runtime"
-	"strconv"
-	"github.com/nthskyradiated/blockchain-in-golang/blockchain"
-	"github.com/nthskyradiated/blockchain-in-golang/utils"
+	"github.com/nthskyradiated/blockchain-in-golang/cli"
+	// "github.com/nthskyradiated/blockchain-in-golang/wallet"
 )
 
-type CommandLine struct {
-	blockchain *blockchain.BlockChain
-}
 
 func main() {
 	defer os.Exit(0)
-	cli := CommandLine{}
+	cli := cli.CommandLine{}
 	cli.Run()
-}
 
-func (cli *CommandLine) printUsage() {
-	fmt.Println("Usage:")
-	fmt.Println("  getbalance -address ADDRESS - Get balance of an address")
-	fmt.Println("  createblockchain -address ADDRESS - Create a blockchain and send genesis block reward to ADDRESS")
-	fmt.Println("  print - Print the blockchain")
-	fmt.Println("  send -from FROM -to TO -amount AMOUNT - Send amount from one address to another")
-}
-
-func (cli *CommandLine) validateArgs() {
-	if len(os.Args) < 2 {
-		cli.printUsage()
-		runtime.Goexit()
-	}
-}
-
-func (cli *CommandLine) printChain() {
-
-	chain := blockchain.ContinueBlockChain("")
-	defer chain.Database.Close()
-	iter := chain.Iterator()
-	fmt.Println("printing")
-	for {
-		block := iter.Next()
-		fmt.Printf("Prev. hash: %x\n", block.PrevHash)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		pow := blockchain.NewProofOfWork(block)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-		if len(block.PrevHash) == 0 {
-			break
-		}
-	}
-}
-
-
-func (cli *CommandLine) createblockchain(address string) {
-
-	chain := blockchain.NewBlockChain(address)
-	chain.Database.Close()
-	fmt.Println("Blockchain created successfully!")
-}
-
-func (cli *CommandLine) getbalance(address string) {
-
-	chain := blockchain.ContinueBlockChain(address)
-	defer chain.Database.Close()
-
-	balance := 0
-	UTXOs := chain.FindUTXOutputs(address)
-
-	for _, out := range UTXOs {
-		balance += out.Value
-	}
-	fmt.Printf("Balance of %s: %d\n", address, balance)
-}
-
-func (cli *CommandLine) send(from, to string, amount int) {
-
-	chain := blockchain.ContinueBlockChain(from)
-	defer chain.Database.Close()
-
-	tx := blockchain.NewTransaction(from, to, amount, chain)
-	chain.AddBlock([]*blockchain.Transaction{tx})
-	fmt.Println("Transaction successful!")
-}
-
-func (cli *CommandLine) Run() {
-	cli.validateArgs()
-	getBalanceCmd := flag.NewFlagSet("getbalance", flag.ExitOnError)
-	createBlockchainCmd := flag.NewFlagSet("createblockchain", flag.ExitOnError)
-	sendCmd := flag.NewFlagSet("send", flag.ExitOnError)
-	printChainCmd := flag.NewFlagSet("print", flag.ExitOnError)
-
-
-	getBalanceAddress := getBalanceCmd.String("address", "", "Address to get balance of")
-	createBlockchainAddress := createBlockchainCmd.String("address", "", "Address to send genesis block reward to")
-	sendFrom := sendCmd.String("from", "", "Address to send from")
-	sendTo := sendCmd.String("to", "", "Address to send to")
-	sendAmount := sendCmd.Int("amount", 0, "Amount to send")
-
-	switch os.Args[1] {
-
-	case "getbalance":
-		err := getBalanceCmd.Parse(os.Args[2:])
-		utils.HandleError(err)
-	
-	case "createblockchain":
-		err := createBlockchainCmd.Parse(os.Args[2:])
-		utils.HandleError(err)
-
-	case "send":
-		err := sendCmd.Parse(os.Args[2:])
-		utils.HandleError(err)
-
-	case "print":
-		err := printChainCmd.Parse(os.Args[2:])
-		utils.HandleError(err)
-
-	default:
-		cli.printUsage()
-		runtime.Goexit()
-	}
-
-	if getBalanceCmd.Parsed() {
-		if *getBalanceAddress == "" {
-			getBalanceCmd.Usage()
-			runtime.Goexit()
-		}
-		cli.getbalance(*getBalanceAddress)
-	}
-
-	if createBlockchainCmd.Parsed() {
-		if *createBlockchainAddress == "" {
-			createBlockchainCmd.Usage()
-			runtime.Goexit()
-		}
-		cli.createblockchain(*createBlockchainAddress)
-	}
-
-	if printChainCmd.Parsed() {
-		cli.printChain()
-	}
-
-	if sendCmd.Parsed() {
-		if *sendFrom == "" || *sendTo == "" || *sendAmount <= 0 {
-			sendCmd.Usage()
-			runtime.Goexit()
-		}
-		cli.send(*sendFrom, *sendTo, *sendAmount)
-	}
+	// w := wallet.CreateWallet()
+	// w.Address()
 }
