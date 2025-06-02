79b9cc32eb4c68e153979fa766f7fc0af2be35e4
andyp <andy.pandaan@outlook.com>
2025-06-02 12:34:47 +0400

    part8 done. merkletree added.

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,8 +1,6 @@
 package blockchain
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"github.com/nthskyradiated/blockchain-in-golang/utils"
 )
 
@@ -15,13 +13,13 @@ type Block struct {
 
 func (b *Block) HashTransactions() []byte {
 	var txHashes [][]byte
-	var txHash [32]byte
+	
 
 	for _, tx := range b.Transactions {
-		txHashes = append(txHashes, tx.ID)
+		txHashes = append(txHashes, tx.Serialize())
 	}
-	txHash = sha256.Sum256(bytes.Join(txHashes, []byte{}))
-	return txHash[:]
+	tree := NewMerkleTree(txHashes)
+	return tree.Root.Data
 }
 
 func CreateBlock(txs []*Transaction, prevHash []byte) *Block {
